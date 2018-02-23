a2d4b3621ac009049797cf35831ef5830b9126f4
Ondrej Kosarko <kosarko@ufal.mff.cuni.cz>
2018-02-23 16:06:48 +0100

    WIP submission workflow

--- dspace-api/src/main/java/cz/cuni/mff/ufal/dspace/submit/step/NarratorStep.java
+++ dspace-api/src/main/java/cz/cuni/mff/ufal/dspace/submit/step/NarratorStep.java
@@ -14,13 +14,24 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.sql.SQLException;
 
+import static org.apache.commons.lang3.StringUtils.isNotBlank;
+
 public class NarratorStep extends AbstractProcessingStep {
     @Override
     public int doProcessing(Context context, HttpServletRequest request, HttpServletResponse response, SubmissionInfo subInfo) throws ServletException, IOException, SQLException, AuthorizeException {
         int resource_id = Util.getIntParameter(request, "submit-narrator-select");
+        String title = request.getParameter("submit-title");
 
         Item submission = subInfo.getSubmissionItem().getItem();
         submission.clearMetadata("viadat", "narrator", Item.ANY, Item.ANY);
+        if(isNotBlank(title)){
+            submission.clearMetadata("dc", "title", Item.ANY, Item.ANY);
+            Metadatum metadatum = new Metadatum();
+            metadatum.schema = "dc";
+            metadatum.element = "title";
+            metadatum.value = title;
+            submission.addMetadatum(metadatum);
+        }
 
         Item narratorTemplate = Item.find(context, resource_id);
         if(narratorTemplate != null){
--- dspace-xmlui/src/main/java/cz/cuni/mff/ufal/dspace/app/xmlui/aspect/submission/submit/NarratorStep.java
+++ dspace-xmlui/src/main/java/cz/cuni/mff/ufal/dspace/app/xmlui/aspect/submission/submit/NarratorStep.java
@@ -3,27 +3,38 @@ package cz.cuni.mff.ufal.dspace.app.xmlui.aspect.submission.submit;
 import cz.cuni.mff.ufal.Logger;
 import org.dspace.app.xmlui.aspect.submission.AbstractSubmissionStep;
 import org.dspace.app.xmlui.utils.UIException;
+import org.dspace.app.xmlui.wing.Message;
 import org.dspace.app.xmlui.wing.WingException;
 import org.dspace.app.xmlui.wing.element.*;
 import org.dspace.authorize.AuthorizeException;
 import org.dspace.content.*;
 import org.dspace.content.Item;
-import org.dspace.discovery.DiscoverQuery;
-import org.dspace.discovery.DiscoverResult;
-import org.dspace.discovery.SearchService;
-import org.dspace.discovery.SearchServiceException;
 import org.dspace.storage.rdbms.DatabaseManager;
 import org.dspace.storage.rdbms.TableRow;
 import org.dspace.storage.rdbms.TableRowIterator;
-import org.dspace.utils.DSpace;
 import org.xml.sax.SAXException;
 
 import java.io.IOException;
 import java.sql.SQLException;
 
+import static org.apache.commons.lang3.StringUtils.isNotBlank;
+
 public class NarratorStep extends AbstractSubmissionStep {
 
     org.apache.log4j.Logger log = Logger.getLogger(NarratorStep.class);
+
+    protected static final Message T_head =
+            message("xmlui.Submission.submit.NarratorStep.head");
+    protected static final Message T_new_narrator =
+            message("xmlui.Submission.submit.NarratorStep.new_narrator");
+    protected static final Message T_title_help =
+            message("xmlui.Submission.submit.NarratorStep.title_help");
+    protected static final Message T_title_label =
+            message("xmlui.Submission.submit.NarratorStep.title_label");
+    protected static final Message T_select_help =
+            message("xmlui.Submission.submit.NarratorStep.select_help");
+    protected static final Message T_select_label =
+            message("xmlui.Submission.submit.NarratorStep.select_label");
 
     public NarratorStep()
     {
@@ -49,11 +60,23 @@ public class NarratorStep extends AbstractSubmissionStep {
         addSubmissionProgressList(div);
 
         List form = div.addList("submit-narrator", List.TYPE_FORM);
-        form.setHead("CHANGE THIS HEAD");
+        form.setHead(T_head);
+
+        String title = item.getMetadata("dc.title");
+        Text textTitle = form.addItem().addText("submit-title");
+        textTitle.setLabel(T_title_label);
+        textTitle.setHelp(T_title_help);
+        if(isNotBlank(title)){
+           textTitle.setValue(title);
+        }
 
 
         Select select = form.addItem().addSelect("submit-narrator-select", "select2");
-        select.addOption(-1, "New narrator CHANGE this");
+        select.setLabel(T_select_label);
+        select.setHelp(T_select_help);
+        select.addOption(-1, T_new_narrator);
+
+        String selected = item.getMetadata("viadat.narrator.identifier");
 
         String query = "select min(f.resource_id) as resource_id, f.text_value as id, s.text_value as name from metadatavalue f join metadatavalue s" +
                 " on f.resource_id = s.resource_id and f.resource_type_id = s.resource_type_id and f.metadata_field_id = ? and s.metadata_field_id = ? group by f.text_value, s.text_value;";
@@ -65,7 +88,7 @@ public class NarratorStep extends AbstractSubmissionStep {
             int resource_id = row.getIntColumn("resource_id");
             String identifier = row.getStringColumn("id");
             String name = row.getStringColumn("name");
-            select.addOption(resource_id, name + " - " + identifier);
+            select.addOption(identifier.equals(selected), resource_id, name + " - " + identifier);
         }
 
         addControlButtons(form);
