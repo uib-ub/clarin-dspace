f609ff4db7318ab05b985a6404cb26397c06d7fe
Ondrej Kosarko <kosarko@ufal.mff.cuni.cz>
2019-11-08 15:35:04 +0100

    An attempt to solve occasional exception
    org.glassfish.jersey.message.internal.WriterInterceptorExecutor$TerminalWriterInterceptor.aroundWriteTo MessageBodyWriter not found for media type=application/json

--- dspace-rest/src/main/java/cz/cuni/mff/ufal/dspace/rest/Application.java
+++ dspace-rest/src/main/java/cz/cuni/mff/ufal/dspace/rest/Application.java
@@ -1,11 +1,13 @@
 package cz.cuni.mff.ufal.dspace.rest;
 
+import org.glassfish.jersey.CommonProperties;
 import org.glassfish.jersey.moxy.json.MoxyJsonFeature;
 import org.glassfish.jersey.moxy.xml.MoxyXmlFeature;
 import org.glassfish.jersey.server.ResourceConfig;
 
 public class Application extends ResourceConfig{
     public Application(){
+        property(CommonProperties.FEATURE_AUTO_DISCOVERY_DISABLE, true);
         register(MoxyJsonFeature.class);
         register(MoxyXmlFeature.class);
         packages("org.dspace.rest","cz.cuni.mff.ufal.dspace.rest");
